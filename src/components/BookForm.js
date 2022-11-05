89ea756d0495c9b7f3e5678aa89e42d80b6c0a00
TemesgenA <temesgen.ayneka@gmail.com>
2022-11-05 22:11:44 +0300

    Code cleanup

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,13 +1,13 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 
 const BookForm = () => (
   <form action="#" method="get" className="form">
-    <label htmlFor="book-title">ADD NEW BOOK</label>
+    <label htmlFor="book-title">
+      ADD NEW BOOK
+      <input type="text" name="book-title" placeholder="Book title" id="book-title" className="form-input-catag" />
+    </label>
     {' '}
     <br />
-    <input type="text" name="book-title" placeholder="Book title" id="book-title" className="form-input-catag" />
-    {/* <input type="text" name="book-catag" id="book-catag" placeholder="Category" /> */}
     <select className="form-input-catag">
       <option value="catagory-1">catagory-1</option>
       <option value="catagory-2">catagory-2</option>
