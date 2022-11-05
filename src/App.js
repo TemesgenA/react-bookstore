c42acfc19f4fd7332968e2d6765c88008ec022cf
TemesgenA <temesgen.ayneka@gmail.com>
2022-11-05 20:17:59 +0300

    Import custom components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Header from './components/Header';
+import Books from './components/Books';
+import BookForm from './components/BookForm';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React!</h1>
+      <Header />
+      <Books />
+      <BookForm />
+
     </div>
   );
 }
