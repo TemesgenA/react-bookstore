8c7d7282695be790b9a6355e677258caccb81982
TemesgenA <temesgen.ayneka@gmail.com>
2022-11-19 11:39:07 +0300

    replace static array with redux store

--- src/components/BookView.js
+++ src/components/BookView.js
@@ -1,26 +1,10 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import BooksList from './Books';
 import BookForm from './BookForm';
 
 const BookView = () => {
-  const books = [
-    {
-      id: 1,
-      title: 'Dune',
-      author: 'Suzanne Collins',
-    },
-    {
-      id: 2,
-      title: 'The Hunger Games',
-      author: 'James Roynolds',
-    },
-    {
-      id: 3,
-      title: 'Catch me if you can',
-      author: 'Leaonardo de Caprio',
-    },
-  ];
-
+  const books = useSelector((state) => state.book);
   return (
     <>
       <BooksList books={books} />
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,13 +1,11 @@
-import { combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 import categoriesReducer from './categories/categories';
 import bookReducer from './books/books';
 
-const rootReducer = combineReducers({
-  Books: bookReducer,
-  Categories: categoriesReducer,
+const store = configureStore({
+  reducer: {
+    book: bookReducer,
+    categories: categoriesReducer,
+  },
 });
-
-const store = configureStore(rootReducer);
-
 export default store;
